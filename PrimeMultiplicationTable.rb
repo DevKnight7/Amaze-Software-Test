6c5ed153d5eb5b4c02e2cd7f6bca17eb32384b97
Ehsan <ehsanatif.sr@gmail.com>
2023-02-14 09:12:40 -0500

    Add following Changes: created helper function for getting and checking Prime Numbers write test cases for helper functions

--- PrimeMultiplicationTable.rb
+++ PrimeMultiplicationTable.rb
@@ -4,4 +4,26 @@ class PrimeMultiplicationTable
   def initialize(number)
     @number = number
   end
+
+  private
+    def prime?(n)
+      return false if n <= 1
+
+      (2..Math.sqrt(n).to_i).each do |i|
+        return false if n % i == 0
+      end
+
+      true
+    end
+
+    def primes
+      primes_arr = []
+      flag = 2
+      while primes_arr.length < number
+        primes_arr << flag if prime?(flag.to_i)
+        flag += 1
+      end
+
+      primes_arr
+    end
 end
--- TestCases/PrimeMultiplicationTableTest.rb
+++ TestCases/PrimeMultiplicationTableTest.rb
@@ -1,5 +1,28 @@
 require 'test/unit'
 require "./PrimeMultiplicationTable"
+require "prime"
 
 class PrimeMultiplicationTableTest < Test::Unit::TestCase
+  def test_is_prime?
+    assert_equal(2.prime?, PrimeMultiplicationTable.new(2).send(:prime?, 2))
+    assert_equal(3.prime?, PrimeMultiplicationTable.new(3).send(:prime?, 3))
+    assert_equal(5.prime?, PrimeMultiplicationTable.new(5).send(:prime?, 5))
+  end
+
+  def test_is_not_prime?
+    assert_equal(1.prime?, PrimeMultiplicationTable.new(1).send(:prime?, 1))
+    assert_equal(4.prime?, PrimeMultiplicationTable.new(4).send(:prime?, 4))
+    assert_equal(6.prime?, PrimeMultiplicationTable.new(6).send(:prime?, 6))
+  end
+
+  def test_primes
+    # positive test case
+    assert_equal(Prime.first(5), PrimeMultiplicationTable.new(5).send(:primes))
+  end
+
+  def test_primes_contain_non_prime_number
+    #negative test case
+    assert_not_equal(Prime.first(4).push(rand(4)*2), PrimeMultiplicationTable.new(5).send(:primes))
+  end
+
 end
